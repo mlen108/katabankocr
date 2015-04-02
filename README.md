d85c07f0e56aa7d7d827b450e9a4ac590276e980
Maciek Lenc <maciej.lenc@sage.com>
2015-04-02 14:46:25 +0100

    Simpler code and comments added too.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# katabankocr
-Code kata for BankOCR
+# KataBankOCR
+
+This is my attempt to code kata for BankOCR use cases. Also, I'm rookie in Ruby!
+
+While the code might be not the most efficient possible, the goal here is slightly
+different = simplicity and readability.
--- user_case_1.rb
+++ user_case_1.rb
@@ -45,15 +45,15 @@ VALUES = {
   " _|" => 9
 }
 
-# split data every 4 lines as each entry is represented in 4 lines.
-data.lines.each_slice(4) do |s|
-  digits = []
-  (0..24).step(3) do |n|
-    out = ""
-    out << s[0].to_s[n, 3]
-    out << s[1].to_s[n, 3]
-    out << s[2].to_s[n, 3]
-    digits << VALUES[out]
+# split data every 4 lines as each entry is represented in 4 lines blocks.
+data.lines.each_slice(4) do |entry|
+  output = []
+  # each line has 27 chars & each digit is represented as 3 chars long
+  # so iterate these numbers and substring at correct step(s).
+  (0..24).step(3) do |idx|
+    digit = entry[0][idx, 3] << entry[1][idx, 3] << entry[2][idx, 3]
+    # find digit representation within our hash map.
+    output << VALUES[digit]
   end
-  puts digits.join
+  puts output.join
 end
