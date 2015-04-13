afcdd88534db56f218103527617a1a7570c7a9a6
Maciek Lenc <maciej.lenc@sage.com>
2015-04-13 08:49:46 +0100

    OCR as a module

--- lib/bank_ocr.rb
+++ lib/bank_ocr.rb
@@ -40,94 +40,96 @@ DIGITS = {
   " _|" => 9
 }
 
-class BankOCR
-  attr_reader :path
+module OCR
+  class Reader
+    attr_reader :path
 
-  def initialize(path = 'data_entries.txt')
-    @path = File.absolute_path(path)
-  end
-
-  def exists?
-    File.exists?(path)
-  end
-
-  def read
-    return unless exists?
-
-    File.read(path)
-  end
-
-  def parse(data = nil)
-    data ||= self.read
-
-    accounts = []
-    klass = AccountEntry.new
-    # split data every 4 lines as each entry is represented in 4 lines blocks.
-    data.lines.each_slice(4) do |entry|
-      accounts << klass.parse(entry)
+    def initialize(path = 'data_entries.txt')
+      @path = File.absolute_path(path)
     end
 
-    accounts
+    def exists?
+      File.exists?(path)
+    end
+
+    def read
+      return unless exists?
+
+      File.read(path)
+    end
+
+    def parse(data = nil)
+      data ||= self.read
+
+      accounts = []
+      klass = Entry.new
+      # split data every 4 lines as each entry is represented in 4 lines blocks.
+      data.lines.each_slice(4) do |entry|
+        accounts << klass.parse(entry)
+      end
+
+      accounts
+    end
   end
-end
 
-class AccountEntry
-  def parse(entry)
-    return unless entry.inject(:+).size == 85
+  class Entry
+    def parse(entry)
+      return unless entry.inject(:+).size == 85
 
-    output = []
-    # each line has 27 chars & each digit is represented as 3 chars long
-    # so iterate these numbers and substring at correct step(s).
-    (0..24).step(3) do |idx|
-      digit = entry[0][idx, 3] << entry[1][idx, 3] << entry[2][idx, 3]
-      # find digit representation within our hash map.
-      if DIGITS.include?(digit)
-        output << DIGITS[digit]
+      output = []
+      # each line has 27 chars & each digit is represented as 3 chars long
+      # so iterate these numbers and substring at correct step(s).
+      (0..24).step(3) do |idx|
+        digit = entry[0][idx, 3] << entry[1][idx, 3] << entry[2][idx, 3]
+        # find digit representation within our hash map.
+        if DIGITS.include?(digit)
+          output << DIGITS[digit]
+        else
+          output << '?'
+        end
+      end
+
+      output.join
+    end
+  end
+
+  class Digit
+    attr_reader :account_number
+
+    def initialize(account_number)
+      if account_number.is_a?(Integer)
+        account_number = account_number.to_s
+      end
+
+      @account_number = account_number
+    end
+
+    def to_s
+      if illegible?
+        "#{account_number} ILL"
+      elsif invalid?
+        "#{account_number} ERR"
       else
-        output << '?'
+        "#{account_number}"
       end
     end
 
-    output.join
-  end
-end
-
-class AccountNumber
-  attr_reader :account_number
-
-  def initialize(account_number)
-    if account_number.is_a?(Integer)
-      account_number = account_number.to_s
+    def checksum
+      (0..account_number.length).reduce(0) do |sum, idx|
+        sum += account_number[idx * -1].to_i * idx
+      end
     end
 
-    @account_number = account_number
-  end
-
-  def to_s
-    if illegible?
-      "#{account_number} ILL"
-    elsif invalid?
-      "#{account_number} ERR"
-    else
-      "#{account_number}"
+    def valid?
+      checksum % 11 == 0
     end
-  end
 
-  def checksum
-    (0..account_number.length).reduce(0) do |sum, idx|
-      sum += account_number[idx * -1].to_i * idx
+    def invalid?
+      !valid?
     end
-  end
 
-  def valid?
-    checksum % 11 == 0
-  end
-
-  def invalid?
-    !valid?
-  end
-
-  def illegible?
-    account_number.include?('?')
+    def illegible?
+      account_number.include?('?')
+    end
   end
 end
--- spec/bank_ocr_spec.rb
+++ spec/bank_ocr_spec.rb
@@ -1,180 +1,182 @@
 require 'bank_ocr'
 
-describe BankOCR do
-  describe '.exists?' do
-    context 'when the file exists' do
-      it { expect(subject.exists?).to be true }
-    end
-
-    context 'when the file does not exist' do
-      subject { BankOCR.new('non_existing_file.txt') }
-
-      it { expect(subject.exists?).to be false }
-    end
-  end
-
-  describe '.read' do
-    context 'when the file is not empty' do
-      it { expect(subject.read).not_to be_empty}
-    end
-
-    context 'when the file does not exist' do
-      subject { BankOCR.new('missing_file.txt') }
-
-      it { expect(subject.read).to be nil}
-    end
-
-    context 'when the file has right content' do
-      it { expect(subject.read).to include ' _  _  _  _  _  _  _  _  _ ' }
-    end
-  end
-
-  describe '.parse' do
-    context 'should parse the entries from string' do
-      subject do
-        new_data =
-        " _  _  _  _  _  _  _  _    \n" +
-        "| || || || || || || || |  |\n" +
-        "|_||_||_||_||_||_||_||_|  |\n" +
-        "\n"
-
-        BankOCR.new.parse(new_data)
+module OCR
+  describe Reader do
+    describe '.exists?' do
+      context 'when the file exists' do
+        it { expect(subject.exists?).to be true }
       end
 
-      it { expect(subject).to match_array(['000000001']) }
+      context 'when the file does not exist' do
+        subject { described_class.new('non_existing_file.txt') }
+
+        it { expect(subject.exists?).to be false }
+      end
     end
 
-    context 'when parses the entries from file' do
-      it 'should contain exact items' do
-        items = [
-          '000000000',
-          '111111111',
-          '123456789',
-          '222222222',
-          '333333333',
-          '444444444',
-          '555555555',
-          '666666666',
-          '777777777',
-          '888888888',
-          '999999999'
-        ]
-        expect(subject.parse).to match_array(items)
+    describe '.read' do
+      context 'when the file is not empty' do
+        it { expect(subject.read).not_to be_empty}
+      end
+
+      context 'when the file does not exist' do
+        subject { described_class.new('missing_file.txt') }
+
+        it { expect(subject.read).to be nil}
+      end
+
+      context 'when the file has right content' do
+        it { expect(subject.read).to include ' _  _  _  _  _  _  _  _  _ ' }
+      end
+    end
+
+    describe '.parse' do
+      context 'should parse the entries from string' do
+        subject do
+          new_data =
+          " _  _  _  _  _  _  _  _    \n" +
+          "| || || || || || || || |  |\n" +
+          "|_||_||_||_||_||_||_||_|  |\n" +
+          "\n"
+
+          described_class.new.parse(new_data)
+        end
+
+        it { expect(subject).to match_array(['000000001']) }
+      end
+
+      context 'when parses the entries from file' do
+        it 'should contain exact items' do
+          items = [
+            '000000000',
+            '111111111',
+            '123456789',
+            '222222222',
+            '333333333',
+            '444444444',
+            '555555555',
+            '666666666',
+            '777777777',
+            '888888888',
+            '999999999'
+          ]
+          expect(subject.parse).to match_array(items)
+        end
       end
     end
   end
-end
 
-describe AccountEntry do
-  describe '.parse' do
-    context 'when the account entry is empty' do
-      subject do
-        empty_entry =
-        ["\n"] +
-        ["\n"] +
-        ["\n"] +
-        ["\n"]
+  describe Entry do
+    describe '.parse' do
+      context 'when the account entry is empty' do
+        subject do
+          empty_entry =
+          ["\n"] +
+          ["\n"] +
+          ["\n"] +
+          ["\n"]
 
-        AccountEntry.new.parse(empty_entry)
+          described_class.new.parse(empty_entry)
+        end
+
+        it { expect(subject).to be nil }
       end
 
-      it { expect(subject).to be nil }
-    end
+      context 'when the account entry is valid' do
+        subject do
+          valid_entry =
+          ["    _  _     _     _  _    \n"] +
+          ["  | _| _||_|| |  | _| _||_|\n"] +
+          ["  ||_  _|  ||_|  ||_  _|  |\n"] +
+          ["\n"]
 
-    context 'when the account entry is valid' do
-      subject do
-        valid_entry =
-        ["    _  _     _     _  _    \n"] +
-        ["  | _| _||_|| |  | _| _||_|\n"] +
-        ["  ||_  _|  ||_|  ||_  _|  |\n"] +
-        ["\n"]
+          described_class.new.parse(valid_entry)
+        end
 
-        AccountEntry.new.parse(valid_entry)
+        it { expect(subject).to eq('123401234') }
       end
 
-      it { expect(subject).to eq('123401234') }
+      context 'when the account entry is invalid' do
+        subject do
+          invalid_entry =
+          [" _  _     _     _  _  #    \n"] +
+          [" _| _||_|| |  | _| _||#|  |\n"] +
+          ["|_  _|  ||_|  ||_  _| #|  |\n"] +
+          ["\n"]
+
+          described_class.new.parse(invalid_entry)
+        end
+
+        it { expect(subject).to eq('2340123?1') }
+      end
     end
+  end
 
-    context 'when the account entry is invalid' do
-      subject do
-        invalid_entry =
-        [" _  _     _     _  _  #    \n"] +
-        [" _| _||_|| |  | _| _||#|  |\n"] +
-        ["|_  _|  ||_|  ||_  _| #|  |\n"] +
-        ["\n"]
+  describe Digit do
+    describe '.initialize' do
+      context 'when the account number is of integer type' do
+        subject { described_class.new(123456789) }
 
-        AccountEntry.new.parse(invalid_entry)
+        it { expect(subject.account_number).to be_instance_of(String) }
       end
 
-      it { expect(subject).to eq('2340123?1') }
-    end
-  end
-end
+      context 'when the account number is of string type' do
+        subject { described_class.new('123456789') }
 
-describe AccountNumber do
-  describe '.initialize' do
-    context 'when the account number is of integer type' do
-      subject { AccountNumber.new(123456789) }
-
-      it { expect(subject.account_number).to be_instance_of(String) }
+        it { expect(subject.account_number).to be_instance_of(String) }
+      end
     end
 
-    context 'when the account number is of string type' do
-      subject { AccountNumber.new('123456789') }
+    describe '.to_s' do
+      context 'when the account number is legible' do
+        subject { described_class.new(123456789) }
 
-      it { expect(subject.account_number).to be_instance_of(String) }
+        it { expect(subject.to_s).to eq('123456789') }
+      end
+
+      context 'when the account number has wrong checksum' do
+        subject { described_class.new(664371495) }
+
+        it { expect(subject.to_s).to eq('664371495 ERR') }
+      end
+
+      context 'when the account number is illegible' do
+        subject { described_class.new('86110??36') }
+
+        it { expect(subject.to_s).to eq('86110??36 ILL') }
+      end
     end
-  end
 
-  describe '.to_s' do
-    context 'when the account number is legible' do
-      subject { AccountNumber.new(123456789) }
+    describe '.checksum' do
+      context 'when the account number has valid checksum' do
+        subject { described_class.new(000000000) }
 
-      it { expect(subject.to_s).to eq('123456789') }
+        it { expect(subject.checksum).to eq(0) }
+      end
     end
 
-    context 'when the account number has wrong checksum' do
-      subject { AccountNumber.new(664371495) }
+    describe '.valid?' do
+      context 'when the account number is valid' do
+        subject { described_class.new(711111111) }
 
-      it { expect(subject.to_s).to eq('664371495 ERR') }
+        it { expect(subject.valid?).to be true }
+      end
     end
 
-    context 'when the account number is illegible' do
-      subject { AccountNumber.new('86110??36') }
+    describe '.invalid?' do
+      context 'when the account number is invalid' do
+        subject { described_class.new(664371495) }
 
-      it { expect(subject.to_s).to eq('86110??36 ILL') }
+        it { expect(subject.invalid?).to be true }
+      end
     end
-  end
 
-  describe '.checksum' do
-    context 'when the account number has valid checksum' do
-      subject { AccountNumber.new(000000000) }
+    describe '.illegible?' do
+      context 'when the number is illegible' do
+        subject { described_class.new('12345678?') }
 
-      it { expect(subject.checksum).to eq(0) }
-    end
-  end
-
-  describe '.valid?' do
-    context 'when the account number is valid' do
-      subject { AccountNumber.new(711111111) }
-
-      it { expect(subject.valid?).to be true }
-    end
-  end
-
-  describe '.invalid?' do
-    context 'when the account number is invalid' do
-      subject { AccountNumber.new(664371495) }
-
-      it { expect(subject.invalid?).to be true }
-    end
-  end
-
-  describe '.illegible?' do
-    context 'when the number is illegible' do
-      subject { AccountNumber.new('12345678?') }
-
-      it { expect(subject.illegible?).to be true }
+        it { expect(subject.illegible?).to be true }
+      end
     end
   end
 end
